2750d91ffea26bbe5d7f1db28e97c0f5e9053fc1
hc3 <eliel.floyd@bol.com.br>
2017-07-23 12:56:32 -0300

    enviando validador de cpf em c

---
+++ hc3/questoes_diego.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hellow World");
+
+    return 0;
+}
--- hc3/validador-cpf.c
+++ hc3/validador-cpf.c
@@ -3,38 +3,48 @@
 #include <string.h>
 
 int main(void) {
-	
+
 	char cpf[11];
 	int i, somaFirstDig,valorConvertido,restoPrimeiroDig,primeiroDig;
 	int j, somaSecondDig,restoSegundoDig,segundoDig;
-	
+	int validadorPrimeiroDig, validadorSegundoDig;
+
 	printf("Digite o numero do CPF: \n");
 	scanf("%s",&cpf);
-	
+
 	somaFirstDig = 0;
-	
+
 	for(i = 0; i < 9; i++) {
 		valorConvertido = cpf[i] - '0';
 		somaFirstDig += (valorConvertido * (10 - i));
-		printf("Digito %d * %d = %d \n",(10 - i),valorConvertido,somaFirstDig);
+		//printf("Digito %d * %d = %d \n",(10 - i),valorConvertido,somaFirstDig);
 	};
 	restoPrimeiroDig = somaFirstDig % 11;
-	printf("Resto do primeiro digito: %d \n",restoPrimeiroDig);
+    //printf("Resto do primeiro digito: %d \n",restoPrimeiroDig);
 	primeiroDig = 11 - restoPrimeiroDig;
-	printf("Primeiro digito encontrado: %d \n",primeiroDig);
-	
+	//printf("Primeiro digito encontrado: %d \n",primeiroDig);
+
 	somaSecondDig = 0;
+
 	for(j = 0; j < 10; j++) {
-		valorConvertido = cpf[i] - '0';
-		somaSecondDig += (valorConvertido * (11 - i));
-		printf("Digito %d * %d = %d \n",(11 - 1),valorConvertido,somaSecondDig);
+		valorConvertido = cpf[j] - '0';
+		somaSecondDig += (valorConvertido * (11 - j));
+		//printf("Digito %d * %d = %d \n",(11 - 1),valorConvertido,somaSecondDig);
 	};
 	restoSegundoDig = somaSecondDig % 11;
-	printf("Resto do segundo digito: %d \n",restoSegundoDig);
+	//printf("Resto do segundo digito: %d \n",restoSegundoDig);
 	segundoDig = 11 - restoSegundoDig;
-	printf("Segundo digito encontrado: %d \n",segundoDig);
-	
-	
+	//printf("Segundo digito encontrado: %d \n",segundoDig);
+
+    validadorPrimeiroDig = cpf[9] - '0';
+    validadorSegundoDig = cpf[10] - '0';
+
+    if((primeiroDig == validadorPrimeiroDig) && (segundoDig == validadorSegundoDig)) {
+        printf("O cpf é valido!");
+    } else {
+        printf("O cpf é invalido");
+    }
+
     return 0;
 
 }
