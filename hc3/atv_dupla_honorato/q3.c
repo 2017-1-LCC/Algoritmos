3b2b2defb45a88dae7fd7a9a7338f69c35afe068
Eliel das Virgens Santos <eliel.floyd@bol.com.br>
2017-08-09 20:12:34 -0300

    Update q3.c
    finalizad atividade

--- hc3/atv_dupla_honorato/q3.c
+++ hc3/atv_dupla_honorato/q3.c
@@ -7,5 +7,24 @@ posição que ele ocupa no vetor.
 #include <stdio.h>
 
 main() {
+  int Q[10], menor, cont=0, posicao;
   
+  for(cont = 0; cont < 10; cont++) {
+  	printf("insira o %d valor: ",cont + 1);
+  	scanf("%d",&Q[cont]);
+  	while(Q[cont] < 0) {
+  		printf("O valor precisa ser maior que 0 \n");
+  		printf("insira o %d valor: ",cont + 1);
+		scanf("%d",&Q[cont]);	
+	}
+	if(cont == 0) {
+  		menor = Q[cont];
+  		posicao = cont;
+	} else if ( Q[cont] < menor) {
+		menor = Q[cont];
+		posicao = cont;
+	}
+  }
+  
+  printf("o valor %d que esta na posicao %d e o menor valor do vetor",menor,posicao);
 }
