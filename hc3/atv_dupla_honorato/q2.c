ac325f658c834cd7055c889972367fc20e101cf6
Eliel das Virgens Santos <eliel.floyd@bol.com.br>
2017-08-09 20:01:14 -0300

    Update q2.c
    alteração feita e sala de aula

--- hc3/atv_dupla_honorato/q2.c
+++ hc3/atv_dupla_honorato/q2.c
@@ -9,5 +9,29 @@ mostrar o vetor e o percentual de alunos aprovados e reprovados.
 #include <stdio.h>
 
 main() {
+  float medias[20];
+  int linha, qntAprovados=0, qntReprovados=0;
+  float porcentAprovados, porcentReprovados;
+  
+  for(linha=0; linha < 20; linha++) {
+  	printf("Digite a %d media da turma: ",linha + 1);
+  	scanf("%f",&medias[linha]);
+  }
+  
+  for(linha=0; linha < 20; linha++) {
+  	if(medias[linha] >= 7) {
+  		printf("A nota %.1f foi aprovada\n", medias[linha]);
+  		qntAprovados++;
+	} else {
+		printf("A nota %.1f foi reprovada\n", medias[linha]);
+		qntReprovados++;
+	}		
+  }
+  
+  porcentAprovados = (qntAprovados * 100 ) / 20;
+  porcentReprovados = (qntReprovados * 100 ) / 20;
+  
+  printf("A porcentagem de aprovados e: %.0f%% \n",porcentAprovados);
+  printf("A porcentagem de aprovados e: %.0f%%",porcentReprovados);
   
 }
